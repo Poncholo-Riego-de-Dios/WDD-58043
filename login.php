182a252b4880ba767de732e8626ee60524cc8d9d
Poncholo Riego de Dios <89110768+Poncholo-Riego-de-Dios@users.noreply.github.com>
2022-11-20 15:29:47 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>My First Log in Page</title>
+</head>
+<body bgcolor = "green">
+<form action = "connection.php" method = "post">
+	<p>
+		<font color = "white" face = "arial">
+		<label>Username</label>
+		<input type = "text" name = "user"/>
+		</font>
+	</p>
+	<p>
+	<label>Password</label>
+		<input type = "password" name = "text"/>
+	</p>
+	<p>
+		<input type = "submit" name = "Login"/>
+	</p>
+</body>
+<?php
+
+
+?>
+</html>
